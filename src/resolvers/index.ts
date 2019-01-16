3de8bdb2506abd7c7cf74661130393478e11440c
Unknown <aman06@gmail.com>
2019-01-16 02:34:37 -0500

    Add Apollo Server

---
+++ src/resolvers/index.ts
@@ -0,0 +1,9 @@
+import { Mutations } from './mutations';
+import { Queries } from './queries';
+import { Subscriptions } from './subscriptions';
+
+export default {
+  Query: Queries,
+  Mutations: Mutations,
+  Subscription: Subscriptions
+};
