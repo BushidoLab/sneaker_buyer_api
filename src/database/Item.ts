2b23e5546d2a28b04aecc8fe153b95994ab46270
Aman Saran <aman06@gmail.com>
2019-01-27 13:44:50 -0500

    issue #6

--- src/database/Item.ts
+++ src/database/Item.ts
@@ -5,18 +5,18 @@ const itemSchema = new Schema({
   size: { type: String, required: true },
   // TODO: Expand on available product types (ie: shoes, shirts, hats, accessories)
   productType: { type: String, required: true },
-  shopifyProductId: { type: String},
+  shopifyProductId: { type: String },
   // Record price for shopify listing
-  shopifyListPrice: { type: Number},
-  stockXProductId: { type: String},
+  shopifyListPrice: { type: Number },
+  stockXProductId: { type: String },
   // Record price for stockx listing
-  stockXListPrice: { type: Number},
+  stockXListPrice: { type: Number },
   title: { type: String, required: true },
   vendor: { type: String, required: true },
   // User will declare item's original purchase price. Used later to calculate profit on item.
   purchasePrice: { type: Number, required: true },
   // Record closing price
-  closingPrice: { type: Number, required: true },
+  closingPrice: { type: Number, required: true }
   // TODO: Declare which storefront item was finally sold on (ie: Item was sold on Shopify or Stockx)
 });
 
