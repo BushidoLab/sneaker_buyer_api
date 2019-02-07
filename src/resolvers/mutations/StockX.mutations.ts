9b67ab0b85bde0dca6b71c8088f1e821052df112
Christopher Arguello <christopheraarguello@gmail.com>
2019-02-07 16:12:47 -0500

    can retrieve selling inventory

--- src/resolvers/mutations/StockX.mutations.ts
+++ src/resolvers/mutations/StockX.mutations.ts
@@ -1,14 +1,28 @@
 import { StockXService } from '../../services';
 
 export const StockX = {
+  // Log-in to StockX with email and password input variables
+  // Returns JWT-AUTH token
   async loginToStockX(_, { email, password }) {
     return new StockXService().login(email, password);
   },
+  // INCOMPLETE
   async addPortfolioItem(_, { token }) {
     return new StockXService().addPortfolioItem(token);
   },
+  // INCOMPLETE
   async deletePortfolioItem(_, { token, id }) {
     return new StockXService().deletePortfolioItem(token, id);
+  },
+  // Log-in to StockX with .ENV variables
+  // TEMPORARY FUNCTION
+  // Returns JWT-AUTH token
+  async loginToStockXWithEnv(_, { email }) {
+    return new StockXService().loginWithEnv(email);
+  },
+  // Retrieve inventory of products for sale.
+  async getCurrentSelling(_, { qty }) {
+    return new StockXService().getCurrentSelling(qty);
   },
   // async ask(_, { token }) {
   //   return new StockXService().ask(token);
--- src/services/StockX.service.ts
+++ src/services/StockX.service.ts
@@ -1,6 +1,6 @@
 import APIBase from './APIBase';
 // import { * } from 'superagent';
-const request = require('superagent')
+const superagent = require('superagent')
 
 export class StockXService extends APIBase {
   constructor() {
@@ -15,30 +15,32 @@ export class StockXService extends APIBase {
   }
 
   public login = async (email: string, password: string): Promise<Error | any> => {
+    let loginResponse;
     try {
       // return this.axios.post('/stage/v1/login', { email, password });
-      return request
+      superagent
       .post(process.env.STOCKX_ENDPOINT + 'login')
       .send({ email: email, password: password })
       .set('x-api-key', process.env.STOCKX_API_KEY)
+      .end((err, res) => {
+        if (err) {
+          return err
+        } else {
+          loginResponse = res.header['jwt-authorization'];
+          return loginResponse;
+        }
+      })
     }
     catch (error) {
       throw Error(`There was an error logging into StockX: ${error}`);
     }
   };
 
-  // public login = async (email: string, password: string): Promise<Error | any> => {
-  //   try {
-  //     return this.axios.post('/stage/v1/login', { email, password });
-  //   } catch (error) {
-  //     throw Error(`There was an error logging into StockX: ${error}`);
-  //   }
-  // };
-
   public addPortfolioItem = async (token: string): Promise<Error | any> => {
+    let apiToken = await this.loginWithEnv(process.env.STOCKX_EMAIL).toString();
     try {
-      return this.axios.post('/public/v1/portfolio', {
-        headers: { 'jwt-authorization': token }
+      return this.axios.post(process.env.STOCKX_ENDPOINT + 'portfolio', {
+        headers: { 'jwt-authorization': apiToken}
       });
     } catch (error) {
       throw Error(`There was an error adding new item to StockX portfolio: ${error}`);
@@ -55,4 +57,50 @@ export class StockXService extends APIBase {
       throw Error(`There was an error deleting portfolio item from StockX: ${error}`);
     }
   };
+
+  public getCurrentSelling = async (qty: string): Promise<Error | any> => {
+    let inventory;
+    try {
+      return superagent
+        .get('https://gateway.stockx.com/public/v1/customers/484296/selling/current?limit=100')
+        // .get(`${URL}/v1/customers/${customerId}/selling/current?limit=500`)
+        .set('jwt-authorization', process.env.STOCKX_API_JWT_TOKEN)
+        .set('x-api-key', process.env.STOCKX_API_KEY)
+        .end((err, res) => {
+          if (err) {
+            return err
+          } else {
+            inventory = res.body;
+            return inventory;
+            // console.log(res.body)
+          }
+        })
+    } catch (error) {
+      throw Error(`There was an error deleting portfolio item from StockX: ${error}`);
+    }
+    // console.log(inventory)
+  };
+
+  public loginWithEnv = async (email: string): Promise<Error | any> => {
+    let loginResponse;
+    try {
+      // return this.axios.post('/stage/v1/login', { email, password });
+      superagent
+      .post(process.env.STOCKX_ENDPOINT + 'login')
+      .send({ email: process.env.STOCKX_EMAIL, password: process.env.STOCKX_PASSWORD })
+      .set('x-api-key', process.env.STOCKX_API_KEY)
+      .end((err, res) => {
+        if (err) {
+          return err
+        } else {
+          loginResponse = res.header['jwt-authorization'];
+          return loginResponse;
+        }
+      })
+    }
+    catch (error) {
+      throw Error(`There was an error logging into StockX: ${error}`);
+    }
+  };
+
 }
